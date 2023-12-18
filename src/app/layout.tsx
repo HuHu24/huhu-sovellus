8c31fef39796c9129472bebb146f893e2454a569
peedudu <66695333+peedudu@users.noreply.github.com>
2023-12-18 22:08:36 +0000

    added the fonts poppins and opensauce

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,17 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
+import localFont from "next/font/local";
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = localFont({ src: '../../public/fonts/Poppins-Bold.ttf',
+  display: 'swap',
+  subsets:['latin'],
+  variable: '--font-poppins',
+})
+const opensauce = localFont({src: '../../public/fonts/OpenSauceOne-Regular.ttf',
+  variable: '--font-opensauce',
+  subsets:['latin'],
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +25,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${opensauce.variable} ${poppins.variable}`}>{children}</body>
     </html>
   )
 }
