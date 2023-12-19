9cede3fdbd09b3cf86883ea5418d02c6162cc62a
peedudu <66695333+peedudu@users.noreply.github.com>
2023-12-19 17:48:47 +0200

    Brandin setuppaaminen #7 (#9)
    * Added the brand colors with their respective names.
    
    * added the fonts poppins and opensauce
    
    * added the fonts poppins and opensauce
    
    * Updated package dependencies, added the fonts under public/fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,24 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      soul: '#b265e6',
+      buenos_aires: '#7ec2f2',
+      tokio: '#fd93ac',
+      barcelona: '#ffe369',
+      oslo: '#253764',
+      helsinki: '#0d0019',
+      ateena: '#ffffff',
+    },
     extend: {
+      fontFamily: {
+        poppins: ['var(--font-poppins)'],
+        opensauce:['var(--font-opensauce)'],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
