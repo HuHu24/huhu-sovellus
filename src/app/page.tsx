863b8758f9a768a2985b8f6bb7367a2aeb138c3d
MitjaWasTaken <mitja.korja@gmail.com>
2024-01-02 16:19:40 +0200

    fix: Fixed some errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 import Head from "next/head"
 
-import { metadata } from "./layout"
 import Navbar from "./components/navbar/navbar"
 
 export default function Home() {
-  let title = metadata.title
+
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-between">
       <Head>
-        <title>{title}</title>
+        <title>Huhu Leirisovellus</title>
       </Head>
       <main className="relative bg-helsinki w-full h-full overflow-hidden flex flex-col items-start justify-start box-border text-left text-[28px] text-palevioletred font-poppins">
          
