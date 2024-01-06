bab5845bb7caa450d23955eb2d251da941e79670
peedudu <66695333+peedudu@users.noreply.github.com>
2024-01-07 01:01:45 +0200

    feat Added the basic tiedote ui that gets the data from tiedot.ts

---
+++ src/app/components/tiedote/tiedot.ts
@@ -0,0 +1,16 @@
+export function getImage(imageId = 's') {
+    return (
+        imageId +
+        '.png'
+    );
+}
+export function otsikko(id ? : string)  {
+    return (
+        'Ruoka peruttu'
+    );
+}
+export function teksti(id ? : string) {
+    return (
+        'Joo safka ei oo hoitanut hommaansa niin kuollaan nälkään. Syy kissan karv...'
+    );
+}
