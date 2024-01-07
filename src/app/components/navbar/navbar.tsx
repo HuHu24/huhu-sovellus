2ac19628d8ac56992fc39e193726c495421eb38a
MitjaWasTaken <mitja.korja@gmail.com>
2024-01-07 15:14:55 +0200

    refactor: Corrected NavbarButton component naming + removed space

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -18,24 +18,24 @@ const Navbar = () => {
     <div className="fixed bottom-0 left-0 w-full flex flex-row items-start justify-start">
         <NavbarMenu items={exampleItems} classes={`${isOpen ? "visible" : "invisible w-0"}`}/>
         <div className="bg-gray w-full h-[70px] overflow-hidden shrink-0 flex flex-row items-center justify-between box-border">
-            <NavBarButton link={'/'} icon={'cottage'}/>
-            <NavBarButton link={'/tiedotteet'} icon={'full_coverage'}/>
-            <NavBarButton link={'/aikataulu'} icon={'event_note'}/>
-            <NavBarButton link={'/kartta'} icon={'distance'}/>
+            <NavbarButton link={'/'} icon={'cottage'}/>
+            <NavbarButton link={'/tiedotteet'} icon={'full_coverage'}/>
+            <NavbarButton link={'/aikataulu'} icon={'event_note'}/>
+            <NavbarButton link={'/kartta'} icon={'distance'}/>
             <div onClick={toggle}>
-                <NavBarButton link={''} icon={'more_horiz'}/>
+                <NavbarButton link={''} icon={'more_horiz'}/>
             </div>
         </div>
     </div>
   )
 }
 
-interface NavBarButtonProps {
+interface NavbarButtonProps {
     link: string,
     icon: string
 }
 
-const NavBarButton = ({link, icon}: NavBarButtonProps) => {
+const NavbarButton = ({link, icon}: NavbarButtonProps) => {
     return (
         <Link href={link} className='flex'>
             <span className='material-symbols-outlined  icon-navbar w-full h-[64px]' >{icon}</span>
