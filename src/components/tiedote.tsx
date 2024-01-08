dd8eb202f7e392f4009998fb5afa910b90b09dae
peedudu <66695333+peedudu@users.noreply.github.com>
2024-01-08 19:18:44 +0200

    Refactor: Added text shadows for tiedotteet

--- src/components/tiedote.tsx
+++ src/components/tiedote.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-
+import image from "next/image"
 const tiedot = {
   otsikko: "Ruoka peruttu",
   teksti:
@@ -12,12 +12,15 @@ const Tiedote = () => {
     <Link href={"/"} className="z-10">
       <div className="mr-4 flex flex-col">
         <div className="flex h-[150px] w-[180px] justify-center overflow-hidden rounded-[20px] border-2 border-helsinki bg-ateena">
-          <img className="h-full" src="/huhu-sovellus/huhuymp.png" />
-        </div>
-        <div className="w-[180px] whitespace-normal font-poppins font-normal text-ateena">
+             <img className="h-full"
+               src="/huhu-sovellus/huhuymp.png"
+               alt=""
+             />
+           </div>
+        <div className="w-[180px] whitespace-normal font-poppins font-normal text-ateena text-shadow">
           {tiedot.otsikko}
         </div>
-        <div className="h-[76px] w-full overflow-hidden whitespace-normal font-opensauce text-sm font-normal text-ateena">
+        <div className="h-[76px] w-full overflow-hidden whitespace-normal font-opensauce text-sm font-normal text-ateena text-shadow">
           {tiedot.teksti} sdf sdf sdklöfjklös klösjkldfjklsdjkl jklsfjkls sdf
           sdf sjksdjfsjkldfsjd sdfsdf sdfsd sdfsdfsdf
         </div>
