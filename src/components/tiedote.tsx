b209dc92b192d6cecb66218594f5085967dc581c
peedudu <66695333+peedudu@users.noreply.github.com>
2024-01-10 22:48:55 +0200

    Style: Added text shadows, Added text shadows to text in tiedote.tsx Bug: Made the drodown menu extend down and not take space from other text inside of the button

--- src/components/tiedote.tsx
+++ src/components/tiedote.tsx
@@ -12,12 +12,12 @@ const Release = () => {
     <Link href={"/"} className="z-10">
       <div className="mr-4 flex flex-col">
         <div className="flex h-[150px] w-[180px] justify-center overflow-hidden rounded-[20px] border-2 border-helsinki bg-ateena">
-          <img className="h-full" src="huhuymp.png" />
+          <img className="h-full" src="huhuymp.png" alt="" />
         </div>
-        <div className="w-[180px] whitespace-normal font-poppins font-normal text-ateena">
+        <div className="w-[180px] whitespace-normal font-poppins font-normal text-ateena text-shadow shadow-helsinki">
           {tiedot.otsikko}
         </div>
-        <div className="h-[76px] w-full overflow-hidden whitespace-normal font-opensauce text-sm font-normal text-ateena">
+        <div className="h-[76px] w-full overflow-hidden whitespace-normal font-opensauce text-sm font-normal text-ateena text-shadow shadow-helsinki">
           {tiedot.teksti} sdf sdf sdklöfjklös klösjkldfjklsdjkl jklsfjkls sdf
           sdf sjksdjfsjkldfsjd sdfsdf sdfsd sdfsdfsdf
         </div>
