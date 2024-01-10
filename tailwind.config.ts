8279db411bb76f92fb1115a49e20b26b2b34210e
peedudu <66695333+peedudu@users.noreply.github.com>
2024-01-10 17:17:22 +0200

    Feat: Added text shadow feature

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss"
+import plugin from 'tailwindcss/plugin';
 
 const config: Config = {
   content: [
@@ -22,6 +23,11 @@ const config: Config = {
         poppins: ["var(--font-poppins)"],
         opensauce: ["var(--font-opensauce)"],
       },
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -29,6 +35,17 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
 }
 export default config
