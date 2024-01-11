521e6d812c1cfa2ca21535a04bef0a52617765ea
peedudu <66695333+peedudu@users.noreply.github.com>
2024-01-11 10:10:43 +0200

    Updated the frontpage and its components (#48)
    * Style: Added text shadows, Added text shadows to text in tiedote.tsx
    Bug: Made the drodown menu extend down and not take space from other text inside of the button
    
    * Style: Formatted with prettier
    
    * Fix: Fixed a build error

--- src/components/daysTimetable.tsx
+++ src/components/daysTimetable.tsx
@@ -8,7 +8,10 @@ const Event = () => {
 
   const arrowStyle = {
     transition: "transform 0.5s",
+    fontSize: "50px",
     transform: isOpen ? "rotate(90deg)" : "rotate(0)",
+    display: "inline-block",
+    overflow: "hidden",
   }
   const extraInfoStyle = {
     transition: isOpen
@@ -22,14 +25,14 @@ const Event = () => {
       <button
         onClick={() => setIsOpen(!isOpen)}
         className={`${
-          isOpen ? "h-[70px]" : "h-11"
-        } relative  flex w-full items-start overflow-hidden rounded-[20px] bg-oslo transition-all duration-300 ease-in-out`}
+          isOpen ? "h-auto" : "h-11"
+        } relative grid w-full items-start overflow-hidden rounded-[20px] bg-oslo transition-all duration-300 ease-in-out`}
       >
-        <div className="flex w-full place-items-center p-2">
+        <div className="place-items-top flex w-full p-2">
           <div className="w-12">
             <div className="font-poppins text-lg">8:30</div>
           </div>
-          <div className="mx-auto font-poppins text-lg font-normal text-ateena">
+          <div className="pointer-events-none mx-auto font-poppins text-lg font-normal text-ateena">
             Aamupala (oispa)
           </div>
           <div className="w-12">
@@ -43,22 +46,25 @@ const Event = () => {
             />
           </div>
         </div>
-      </button>
-      <div className="absolute bottom-0">
-        <div
-          style={extraInfoStyle}
-          className="pointer-events-none p-2 text-left"
-        >
-          <p className="pointer-events-none">Extra info here</p>
+        <div className="pointer-events-none relative bottom-0 flex overflow-auto">
+          <div
+            style={extraInfoStyle}
+            className="pointer-events-none  text-left"
+          >
+            <p className="pointer-events-none break-all p-2 text-left">
+              Extra info
+              hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+              Extra info
+              hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+            </p>
+          </div>
         </div>
-      </div>
+      </button>
     </div>
   )
 }
 
 const DaysTimetable = () => {
-  const id = 1
-
   return (
     <div className="z-10 flex flex-col gap-2">
       <div className=" flex h-12 items-center justify-center gap-2.5 rounded-[20px] bg-soul font-poppins text-2xl text-ateena">
