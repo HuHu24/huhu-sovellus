2cdcc03b0fb83c2993fc96493cb913c750c96b6b
peedudu <66695333+peedudu@users.noreply.github.com>
2024-01-18 13:48:17 +0200

    Refactor: Prettier

--- src/app/releases/release/page.tsx
+++ src/app/releases/release/page.tsx
@@ -1,7 +1,5 @@
-import Release_fullscreen from "@/components/release/release_fullscreen";
+import Release_fullscreen from "@/components/release/release_fullscreen"
 
 export default function Home() {
-    return (
-        <Release_fullscreen></Release_fullscreen>
-    )
+  return <Release_fullscreen></Release_fullscreen>
 }
--- src/components/release/release_fullscreen.tsx
+++ src/components/release/release_fullscreen.tsx
@@ -1,57 +1,76 @@
 "use client"
-import { useState } from 'react';
+import { useState } from "react"
 import Link from "next/link"
 
 const Release_fullscreen = () => {
-    const [lightMode, setLightMode] = useState(false);
+  const [lightMode, setLightMode] = useState(false)
 
-    const toggleLightMode = () => {
-        setLightMode(!lightMode);
-    }
+  const toggleLightMode = () => {
+    setLightMode(!lightMode)
+  }
 
-    
-    return (
-        <div className={`w-screen h-screen ${lightMode ? 'bg-ateena' : 'bg-helsinki'} overflow-x-hidden overflow-y-auto mb-4`}>
-            <div
-                className={`h-1/4 w-full bg-ateena align-top flex justify-center items-center`}>
-                <img src="huhuymp.png" className="h-full align-middle" alt=""/>
-                <Link href={"/"} className="z-10">
-                    <div
-                        className="fixed top-0 left-0 text-shadow shadow-buenos_aires material-symbols-outlined text-[49px] text-buenos_aires">
-                        arrow_left_alt
-                    </div>
-                </Link>
+  return (
+    <div
+      className={`h-screen w-screen ${
+        lightMode ? "bg-ateena" : "bg-helsinki"
+      } mb-4 overflow-y-auto overflow-x-hidden`}
+    >
+      <div
+        className={`flex h-1/4 w-full items-center justify-center bg-ateena align-top`}
+      >
+        <img src="huhuymp.png" className="h-full align-middle" alt="" />
+        <Link href={"/"} className="z-10">
+          <div className="material-symbols-outlined fixed left-0 top-0 text-[49px] text-buenos_aires shadow-buenos_aires text-shadow">
+            arrow_left_alt
+          </div>
+        </Link>
+      </div>
+      <div
+        className={`relative ml-6 h-full w-full ${
+          lightMode ? "text-helsinki" : "text-ateena"
+        }`}
+      >
+        <div className="mt-3 w-screen">
+          <div className="flex justify-between">
+            <div className="grid whitespace-normal font-poppins text-5xl">
+              {" "}
+              Jaahas{" "}
             </div>
-            <div className={`h-full w-full ml-6 relative ${lightMode ? 'text-helsinki' : 'text-ateena'}`}>
-                <div className="w-screen mt-3">
-                    <div className="flex justify-between">
-                        <div className="whitespace-normal text-5xl font-poppins grid"> Jaahas </div>
-                        <button onClick={toggleLightMode} className={`material-symbols-outlined text-5xl mr-8 ${lightMode ? 'text-helsinki' : 'text-ateena'}`}>
-                            {lightMode ? 'dark_mode' : 'light_mode'}
-                        </button>
-                    </div>
-                    <div
-                        className="whitespace-normal text-2xl font-poppins mt-4 ml-0 "> 1.11.1111
-                        22:22
-                    </div>
-                    <div
-                        className="whitespace-normal text-2xl font-poppins ml-0"> Julkaisija:
-                        Chatgpt..
-                    </div>
-                </div>
-                <div className="text-xl break-words mr-10 overflow-auto mt-2.5">
-                    Moro kaikki!
-                    Joudumme vähän pettämään teidän odotuksia – meidän suunnitellut herkkutreffit joudutaan siirtämään. Pahoittelut tästä, mutta meillä on ollut muutama logistinen sotku, ja keittiöväki on vähän pulassa.
-                    Tässä pikainen debrief:
-                    Toimitusseikkailut: Kaikki tilaukset eivät saapuneet, ja ne, jotka saapuivat, näyttivät siltä kuin ne olisivat kierrelleet maailmaa ennen perille pääsyään.
-                    Henkilökunta puuttuu: Jotkut kokit päättivät ottaa vapaapäivän samaan aikaan, joten meillä on enemmän keittiöhaasteita kuin voimme sanoa &quot;keittiö&quot;.
-                    Yritetään paikata hommaa ja järkätä teille jotain parempaa. Pysykää kuulolla ja pysykää nälkäisinä!
-                    Kysymyksiä? Ota yhteyttä meihin. Olemme täällä sinua varten.
-                    <p>Kuva: Screenshot brandikirjasta</p>
-                </div>
-            </div>
+            <button
+              onClick={toggleLightMode}
+              className={`material-symbols-outlined mr-8 text-5xl ${
+                lightMode ? "text-helsinki" : "text-ateena"
+              }`}
+            >
+              {lightMode ? "dark_mode" : "light_mode"}
+            </button>
+          </div>
+          <div className="ml-0 mt-4 whitespace-normal font-poppins text-2xl ">
+            {" "}
+            1.11.1111 22:22
+          </div>
+          <div className="ml-0 whitespace-normal font-poppins text-2xl">
+            {" "}
+            Julkaisija: Chatgpt..
+          </div>
         </div>
-    )
+        <div className="mr-10 mt-2.5 overflow-auto break-words text-xl">
+          Moro kaikki! Joudumme vähän pettämään teidän odotuksia – meidän
+          suunnitellut herkkutreffit joudutaan siirtämään. Pahoittelut tästä,
+          mutta meillä on ollut muutama logistinen sotku, ja keittiöväki on
+          vähän pulassa. Tässä pikainen debrief: Toimitusseikkailut: Kaikki
+          tilaukset eivät saapuneet, ja ne, jotka saapuivat, näyttivät siltä
+          kuin ne olisivat kierrelleet maailmaa ennen perille pääsyään.
+          Henkilökunta puuttuu: Jotkut kokit päättivät ottaa vapaapäivän samaan
+          aikaan, joten meillä on enemmän keittiöhaasteita kuin voimme sanoa
+          &quot;keittiö&quot;. Yritetään paikata hommaa ja järkätä teille jotain
+          parempaa. Pysykää kuulolla ja pysykää nälkäisinä! Kysymyksiä? Ota
+          yhteyttä meihin. Olemme täällä sinua varten.
+          <p>Kuva: Screenshot brandikirjasta</p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default Release_fullscreen;
+export default Release_fullscreen
