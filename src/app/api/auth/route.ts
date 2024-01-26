e1a261c83f00b17dd31b88a9ff7a2b2c20c3e5c9
Aaro_laptop <aaro.heroja@gmail.com>
2024-01-26 22:56:38 +0200

    feat: auth verifying endpoint

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -3,7 +3,25 @@ import { cookies, headers } from "next/headers"
 import { NextRequest, NextResponse } from "next/server"
 import { initFirebaseAdmin } from "@/firebaseAdmin"
 
-// Init the Firebase SDK every time the server is called
+export async function GET(request: NextRequest) {
+  await initFirebaseAdmin()
+
+  const session = cookies().get("session")?.value || ""
+
+  //Validate if the cookie exist in the request
+  if (!session) {
+    return NextResponse.json({ isLogged: false }, { status: 401 })
+  }
+
+  //Use Firebase Admin to validate the session cookie
+  const decodedClaims = await auth().verifySessionCookie(session, true)
+
+  if (!decodedClaims) {
+    return NextResponse.json({ isLogged: false }, { status: 401 })
+  }
+
+  return NextResponse.json({ isLogged: true }, { status: 200 })
+}
 
 export async function POST(request: NextRequest, response: NextResponse) {
   await initFirebaseAdmin()
