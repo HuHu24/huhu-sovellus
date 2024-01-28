4c38e6b31d636e7368dc552021925dc9a7bc5afb
peedudu <66695333+peedudu@users.noreply.github.com>
2024-01-28 19:45:59 +0200

    Bug: Allowed scrolling on small screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@ import DaysTimetable from "@/components/daysTimetable"
 import Releases from "@/components/release/release_frontpage"
 export default function Home() {
   return (
-    <div className="relative h-full w-full overflow-hidden bg-helsinki">
+    <div className="relative h-full w-full overflow-x-hidden overflow-y-scroll bg-helsinki">
       <div className="absolute -right-40 -top-40 p-2.5">
         <div className="inline-flex h-[369px] w-[360px] items-end justify-start gap-2.5">
           <div className="shrink grow basis-0 self-stretch rounded-full bg-barcelona" />
         </div>
       </div>
-      <div className="absolute -bottom-48 -left-48 p-2.5 ">
+      <div className="fixed -bottom-40 -left-44 p-2.5 ">
         <div className="inline-flex h-[341px] w-[341px] flex-col items-end justify-start gap-2.5">
           <div className="shrink grow basis-0 self-stretch rounded-full bg-soul" />
         </div>
