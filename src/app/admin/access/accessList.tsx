0d1cf8aba3bc0d1637dae6018e2ad9d6f0d5ff64
Aaro <aaro.heroja@gmail.com>
2024-02-02 17:18:25 +0200

    feat: api endpoint adding user to claims list

--- src/app/admin/access/accessList.tsx
+++ src/app/admin/access/accessList.tsx
@@ -1,27 +1,29 @@
-import React from "react";
+import React from "react"
 
-function AccessList(props: { accessList: [{ email: string; uid: string }] | [], accessListHeading: string}) {
+function AccessList(props: {
+  accessList: [{ email: string; uid: string }] | []
+  accessListHeading: string
+}) {
   if (props.accessList.length != 0) {
-    return <div className="z-10 flex flex-col gap-2 w-full max-w-[500px]">
-      <h2
-        className="font-bold flex h-12 items-center justify-center gap-2.5 rounded-[20px] bg-soul font-poppins text-2xl text-ateena">
-        {props.accessListHeading}
-      </h2>
-      <>
-        {
-          props.accessList.map((admin) => <>
-            <div
-              className="h-11 rounded-[20px] bg-oslo place-items-top flex w-full p-2"
-            >
-              <h3 className="font-poppins text-lg ">{admin.email}</h3>
-            </div>
-          </>)
-        }
-      </>
-    </div>;
+    return (
+      <div className="z-10 flex w-full max-w-[500px] flex-col gap-2">
+        <h2 className="flex h-12 items-center justify-center gap-2.5 rounded-[20px] bg-soul font-poppins text-2xl font-bold text-ateena">
+          {props.accessListHeading}
+        </h2>
+        <>
+          {props.accessList.map((admin) => (
+            <>
+              <div className="place-items-top flex h-11 w-full rounded-[20px] bg-oslo p-2">
+                <h3 className="font-poppins text-lg ">{admin.email}</h3>
+              </div>
+            </>
+          ))}
+        </>
+      </div>
+    )
   }
 
   return <></>
 }
 
-export default AccessList;
+export default AccessList
--- src/app/api/auth/claims/route.ts
+++ src/app/api/auth/claims/route.ts
@@ -1,10 +1,31 @@
-import { auth } from "firebase-admin"
+import { auth, firestore } from "firebase-admin"
 import { NextRequest, NextResponse } from "next/server"
+import { getFirestore } from "firebase/firestore"
+import { initFirebaseAdmin } from "@/firebaseAdmin"
 
 export async function POST(request: NextRequest) {
   try {
-    const result = (await request.json()) as { role: "admin"; uid: string }
-    await auth().setCustomUserClaims(result.uid, { admin: true })
+    const result = (await request.json()) as {
+      role: "admin" | "safety" | "subcamp"
+      uid: string
+    }
+
+    await initFirebaseAdmin()
+
+    const user = (await auth().getUser(result.uid)) as { email: string }
+
+    const claimsDoc = await firestore().doc("/claims/claims").get()
+    let data = claimsDoc.data() as {
+      admin: [{ email: string; uid: string }]
+      safety: [{ email: string; uid: string }]
+      subcamp: [{ email: string; uid: string }]
+    }
+
+    data[result.role].push({ email: user.email, uid: result.uid })
+    await firestore().doc("/claims/claims").set(data)
+
+    const customClaims = { [result.role]: true }
+    await auth().setCustomUserClaims(result.uid, customClaims)
   } catch (e) {
     return NextResponse.json(
       {
