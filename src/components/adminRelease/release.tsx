87295c21c891e6cf01857c204b70094c575c794b
peedudu <66695333+peedudu@users.noreply.github.com>
2024-03-08 13:56:15 +0200

    Format: Prettier

--- src/components/adminRelease/release.tsx
+++ src/components/adminRelease/release.tsx
@@ -1,67 +1,83 @@
 "use client"
 
-import Link from "next/link";
-import {useState} from "react";
+import Link from "next/link"
+import { useState } from "react"
 
 const tiedot = {
-    id: 1,
-    otsikko: "Ruoka peruttu",
-    teksti:
-        "Joo safka ei oo hoitanut hommaansa niin kuollaan nälkään. Syy kissan karv...",
-    kuva: "huhuymp.png",
-    julkaisija: "Minä/osaalue",
-    kriittisyys: "Kriittinen",
-    julkaisupvm: "6.9.2222",
+  id: 1,
+  otsikko: "Ruoka peruttu",
+  teksti:
+    "Joo safka ei oo hoitanut hommaansa niin kuollaan nälkään. Syy kissan karv...",
+  kuva: "huhuymp.png",
+  julkaisija: "Minä/osaalue",
+  kriittisyys: "Kriittinen",
+  julkaisupvm: "6.9.2222",
 }
 export const Release = () => {
-    const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
 
-    return (
-        <div className="z-10">
-            <div className="flex items-center bg-ateena bg-opacity-0">
-                <Link href={"./releases/release"} className="flex h-[140px] w-[180px] justify-center overflow-hidden rounded-[20px] border-2 border-helsinki bg-ateena">
-                    <img className="h-full" src="huhuymp.png" alt="" />
-                </Link>
-                <div className="ml-1 flex w-[200px] flex-col justify-center">
-                    <div
-                        className="text-overflow-ellipsis overflow-hidden whitespace-nowrap break-all font-poppins text-[18px] text-ateena shadow-helsinki text-shadow">
-                        {tiedot.otsikko}
-                    </div>
-                    <div onClick={() => setIsOpen(!isOpen)} className="break-all font-opensauce text-[15px] shadow-helsinki text-shadow">
-                        {"julkaistu: " + tiedot.julkaisupvm}
-                        <br/>
-                        {"julkaisija: " + tiedot.julkaisija}
-                        <br/>
-                        {"kriittisyys: "+tiedot.kriittisyys}
-                    </div>
-                    <button
-                        onClick={() => setIsOpen(!isOpen)}
-                        className="mt-1 material-symbols-outlined z-20 text-[49px] text-tokio flex justify-between"
-                    >
-                        <a>visibility_lock</a>
-                        <a >keyboard_arrow_down</a>
-                    </button>
-
-
-                </div>
-
-            </div>
-            {isOpen ? (
-                <div className="relative z-10 w-full h-[50px] bg-soul flex rounded-[10px]"
-                     style={{justifyContent: 'space-between'}}>
-                    <div className="flex" style={{justifyContent: 'space-between'}}>
-                        <Link href={"./releases/release/edit"}
-                              className="material-symbols-outlined text-ateena text-[50px]">visibility</Link>
-                        <p className="text-[40px] -mt-1.5 ml-2 font-opensauce">200</p>
-                    </div>
-                    <div>
-                        <Link href={"./notifications/release/"}
-                              className="material-symbols-outlined text-ateena text-[50px]">notifications_active</Link>
-                        <Link href={"./releases/release"}
-                              className="material-symbols-outlined text-ateena mr-2 text-[50px]">edit</Link>
-                    </div>
-                </div>
-            ) : null}
+  return (
+    <div className="z-10">
+      <div className="flex items-center bg-ateena bg-opacity-0">
+        <Link
+          href={"./releases/release"}
+          className="flex h-[140px] w-[180px] justify-center overflow-hidden rounded-[20px] border-2 border-helsinki bg-ateena"
+        >
+          <img className="h-full" src="huhuymp.png" alt="" />
+        </Link>
+        <div className="ml-1 flex w-[200px] flex-col justify-center">
+          <div className="text-overflow-ellipsis overflow-hidden whitespace-nowrap break-all font-poppins text-[18px] text-ateena shadow-helsinki text-shadow">
+            {tiedot.otsikko}
+          </div>
+          <div
+            onClick={() => setIsOpen(!isOpen)}
+            className="break-all font-opensauce text-[15px] shadow-helsinki text-shadow"
+          >
+            {"julkaistu: " + tiedot.julkaisupvm}
+            <br />
+            {"julkaisija: " + tiedot.julkaisija}
+            <br />
+            {"kriittisyys: " + tiedot.kriittisyys}
+          </div>
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="material-symbols-outlined z-20 mt-1 flex justify-between text-[49px] text-tokio"
+          >
+            <a>visibility_lock</a>
+            <a>keyboard_arrow_down</a>
+          </button>
         </div>
-    )
+      </div>
+      {isOpen ? (
+        <div
+          className="relative z-10 flex h-[50px] w-full rounded-[10px] bg-soul"
+          style={{ justifyContent: "space-between" }}
+        >
+          <div className="flex" style={{ justifyContent: "space-between" }}>
+            <Link
+              href={"./releases/release/edit"}
+              className="material-symbols-outlined text-[50px] text-ateena"
+            >
+              visibility
+            </Link>
+            <p className="-mt-1.5 ml-2 font-opensauce text-[40px]">200</p>
+          </div>
+          <div>
+            <Link
+              href={"./notifications/release/"}
+              className="material-symbols-outlined text-[50px] text-ateena"
+            >
+              notifications_active
+            </Link>
+            <Link
+              href={"./releases/release"}
+              className="material-symbols-outlined mr-2 text-[50px] text-ateena"
+            >
+              edit
+            </Link>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  )
 }
--- src/components/adminRelease/sort.tsx
+++ src/components/adminRelease/sort.tsx
@@ -3,57 +3,64 @@
 import React, { useState } from "react"
 
 interface SortProps {
-    title: string;
-    options: string[];
+  title: string
+  options: string[]
 }
 
-const Sort: React.FC<SortProps> = ({title, options }) => {
-    const [isOpen, setIsOpen] = useState(false)
-    const [selectedOptions, setSelectedOptions] = useState<string[]>([])
+const Sort: React.FC<SortProps> = ({ title, options }) => {
+  const [isOpen, setIsOpen] = useState(false)
+  const [selectedOptions, setSelectedOptions] = useState<string[]>([])
 
-    const handleSort = (option: string) => {
-        if (selectedOptions.includes(option)) {
-            setSelectedOptions(selectedOptions.filter(o => o !== option))
-        } else {
-            setSelectedOptions([...selectedOptions, option])
-        }
-        setIsOpen(true);
-        console.log(option)
+  const handleSort = (option: string) => {
+    if (selectedOptions.includes(option)) {
+      setSelectedOptions(selectedOptions.filter((o) => o !== option))
+    } else {
+      setSelectedOptions([...selectedOptions, option])
     }
+    setIsOpen(true)
+    console.log(option)
+  }
 
-    return (
-        <div className="relative w-1/3 z-20">
-            <button
-                onClick={() => setIsOpen(!isOpen)}
-                className={`relative grid w-full items-start overflow-hidden transition-all bg-tokio rounded-t-[20px] ${isOpen ? '' : 'rounded-b-[20px] delay-300'} duration-500 ease-in-out`}
-            >
-                <div
-                    className=" h-7 px-[11px] py-0.5 rounded-[20px] shadow justify-between items-center inline-flex">
-                    <div
-                        className="w-[60px] h-3 text-helsinki text-sm font-normal font-poppins leading-[10px]">{title}
-                    </div>
-                    <div className="w-6 h-6 relative">
-                        <div className="material-symbols-outlined h-6 left-0 top-0 text-helsinki">
-                            <a>keyboard_arrow_down</a>
-                        </div>
-                    </div>
-                </div>
-            </button>
-            <div
-                className={`text-left h-auto relative grid w-full items-start overflow-hidden transition-all ${isOpen ? 'max-h-[500px] rounded-b-[20px] delay-250' : 'max-h-0'} duration-500 ease-in-out transition-max-height`}
-            >
-                {options.map((option) => (
-                    <button
-                        key={option}
-                        onClick={() => handleSort(option)}
-                        className={`break-all p-2 text-left  w-full  ${selectedOptions.includes(option) ? "bg-gray text-ateena" : "bg-tokio text-helsinki"}`}
-                    >
-                        {" "+option}
-                    </button>
-                ))}
+  return (
+    <div className="relative z-20 w-1/3">
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className={`relative grid w-full items-start overflow-hidden rounded-t-[20px] bg-tokio transition-all ${
+          isOpen ? "" : "rounded-b-[20px] delay-300"
+        } duration-500 ease-in-out`}
+      >
+        <div className=" inline-flex h-7 items-center justify-between rounded-[20px] px-[11px] py-0.5 shadow">
+          <div className="h-3 w-[60px] font-poppins text-sm font-normal leading-[10px] text-helsinki">
+            {title}
+          </div>
+          <div className="relative h-6 w-6">
+            <div className="material-symbols-outlined left-0 top-0 h-6 text-helsinki">
+              <a>keyboard_arrow_down</a>
             </div>
+          </div>
         </div>
-    )
+      </button>
+      <div
+        className={`relative grid h-auto w-full items-start overflow-hidden text-left transition-all ${
+          isOpen ? "delay-250 max-h-[500px] rounded-b-[20px]" : "max-h-0"
+        } transition-max-height duration-500 ease-in-out`}
+      >
+        {options.map((option) => (
+          <button
+            key={option}
+            onClick={() => handleSort(option)}
+            className={`w-full break-all p-2  text-left  ${
+              selectedOptions.includes(option)
+                ? "bg-gray text-ateena"
+                : "bg-tokio text-helsinki"
+            }`}
+          >
+            {" " + option}
+          </button>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Sort
