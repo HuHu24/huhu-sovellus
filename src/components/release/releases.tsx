d619ee0ad9d864f3194cc69f11d1ef73c9954c8c
peedudu <66695333+peedudu@users.noreply.github.com>
2024-03-22 13:22:28 +0200

    Feat: components for releases

--- src/components/release/releases.tsx
+++ src/components/release/releases.tsx
@@ -1,44 +1,31 @@
-import { Release } from "@/components/release/release"
+"use client"
+import { useEffect, useState } from 'react';
+import { getAllReleases } from '@/firebase';
+import { Release } from '@/components/release/release';
+interface releaseData {
+    id: string;
+}
 
 const Releases = () => {
+    const [releases, setReleases] = useState<releaseData[]>([]);
+  useEffect(() => {
+    const fetchReleases = async () => {
+      const allReleases = await getAllReleases();
+      setReleases(allReleases);
+    };
+
+    fetchReleases().catch(console.error);
+  }, []);
+
   return (
     <div className="w-full overflow-y-auto">
       <div className="grid grid-cols-1 items-center gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
-        <Release />
+        {releases.map((release) => (
+          <Release key={release.id} id={release.id} />
+        ))}
       </div>
     </div>
-  )
-}
-export default Releases
+  );
+};
+
+export default Releases;
