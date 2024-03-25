589dfaf359037159b95d98d8380a4653b3b34bf1
peedudu <66695333+peedudu@users.noreply.github.com>
2024-03-25 13:45:46 +0200

    Refactor:; Now uses @/env

--- src/app/auth/signout/route.tsx
+++ src/app/auth/signout/route.tsx
@@ -1,11 +1,12 @@
 import { NextRequest, NextResponse } from "next/server"
 import { cookies } from "next/headers"
 import { signOut } from "@/firebase"
+import {env} from "@/env"
 
 export async function GET(request: NextRequest) {
   cookies().delete("session")
   await signOut()
   return NextResponse.redirect(
-    new URL(`${process.env.NEXT_PUBLIC_URL}/subcamp`, request.url)
+    new URL(`${env.NEXT_PUBLIC_URL}/subcamp`, request.url)
   )
 }
