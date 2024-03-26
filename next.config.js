557c8cc10e08e96cbd55f72f2d026bf055677dce
peedudu <66695333+peedudu@users.noreply.github.com>
2024-03-26 14:56:08 +0200

    Feat: Introduced PWA functionality Refactor: Removed pages created in public by firebase

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("@ducanh2912/next-pwa").default({
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  dest: "public",
+})
 const nextConfig = {
-  /**
-   * This is here so GitHub pages can handle the deployment and if this repository is deployed somewhere else, changed the basePath.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
   images: {
     unoptimized: true,
   },
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
