bf3ce9c151bdc203712e27c5f8131e1a0af80ab7
peedudu <66695333+peedudu@users.noreply.github.com>
2024-03-28 17:12:04 +0200

    Feat: Automaticly subscribes to the topic corresponding to users subcamp upon subcamp selection

--- src/app/api/auth/claims/subcamp/route.ts
+++ src/app/api/auth/claims/subcamp/route.ts
@@ -1,51 +1,41 @@
 import { auth } from "firebase-admin"
 import { NextRequest, NextResponse } from "next/server"
-import { getDecodedClaims } from "@/firebaseAdmin"
+import { getDecodedClaims, subscribeToTopic } from "@/firebaseAdmin"
 import { cookies } from "next/headers"
 
 export async function POST(request: NextRequest) {
-  try {
-    const result = (await request.json()) as { subcamp: number }
-    if (!result.subcamp || result.subcamp < 1 || result.subcamp > 6) {
-      return NextResponse.json(
-        {
-          message:
-            "Error occurred when processing custom claim. Check request body formatting and data",
-        },
-        { status: 400 }
-      )
-    }
+  const result = (await request.json()) as { subcamp: number, token: string }
+  const decodedClaims = await getDecodedClaims(cookies().get("session")?.value || "")
 
-    const decodedClaims = await getDecodedClaims(
-      cookies().get("session")?.value || ""
-    )
-    if (!decodedClaims?.uid) {
-      return NextResponse.json(
-        {
-          message: "There was an error with your session",
-        },
-        { status: 400 }
-      )
-    }
+  if (!isValidSubcamp(result.subcamp) || !decodedClaims?.uid) {
+    return errorResponse()
+  }
 
-    await auth().setCustomUserClaims(decodedClaims.uid, {
-      subcamp: result.subcamp,
-    })
+  await subscribeToTopic(result.token, result.subcamp.toString())
+  await auth().setCustomUserClaims(decodedClaims.uid, { subcamp: result.subcamp })
 
-    return NextResponse.json(
+  return successResponse(result.subcamp)
+}
+
+function isValidSubcamp(subcamp: number) {
+  return subcamp && subcamp >= 1 && subcamp <= 6
+}
+
+function errorResponse() {
+  return NextResponse.json(
       {
-        message: "Subcamp selected",
-        subcamp: result.subcamp,
-      },
-      { status: 200 }
-    )
-  } catch (e) {
-    return NextResponse.json(
-      {
-        message:
-          "Error occurred when processing custom claim. Check request body formatting and data",
+        message: "Error occurred when processing custom claim. Check request body formatting and data",
       },
       { status: 400 }
-    )
-  }
+  )
+}
+
+function successResponse(subcamp: number) {
+  return NextResponse.json(
+      {
+        message: "Subcamp selected",
+        subcamp: subcamp,
+      },
+      { status: 200 }
+  )
 }
