8bcab3d38dbc7718014316bc691fbae0ecef3ffb
peedudu <66695333+peedudu@users.noreply.github.com>
2024-03-28 17:12:45 +0200

    Feat: Push notifications

--- src/firebase.ts
+++ src/firebase.ts
@@ -18,12 +18,14 @@ import {
 } from "@firebase/auth"
 import { env } from "@/env"
 import { getDoc } from "@firebase/firestore"
+import {getMessaging, getToken, isSupported, onMessage} from "firebase/messaging";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyAGDOLaayTJxdlYWGfCscQ3Bp4e4md7r_o",
   authDomain: "huhu-sovellus.firebaseapp.com",
   projectId: "huhu-sovellus",
@@ -39,6 +41,7 @@ const app = initializeApp(firebaseConfig)
 
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app)
+export const messaging = async () => await isSupported() && getMessaging(app)
 export const auth = getAuth(app)
 
 export const loginWithEmailAndPassword = async (
@@ -121,3 +124,4 @@ export const getAllReleases = async () => {
   const releases = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
   return releases
 }
+
