f73ee006d1fca0c31b24d13e40caf9da50832802
Aromi <aaro.heroja@gmail.com>
2024-04-06 14:16:25 +0300

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,12 +3,10 @@ import React, { useState, useEffect } from "react"
 import { auth } from "@/firebase"
 import { onAuthStateChanged } from "firebase/auth"
 import Link from "next/link"
-
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false)
   const [allowOpen, setAllowOpen] = useState(true)
   const [isAuthenticated, setIsAuthenticated] = useState(false)
-
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user && user.email) {
@@ -18,42 +16,8 @@ const Navbar = () => {
       }
     })
   }, [])
-
   return (
     <div className="fixed bottom-0 left-0 z-20 flex w-full flex-row items-start justify-start">
-      <div
-        className="box-border flex h-[70px] w-full shrink-0 flex-row items-center justify-between overflow-hidden bg-gray px-4"
-        onClick={toggle}
-      >
-        <Link href="/" className="flex">
-          <span className="material-symbols-outlined h-[48px] w-full text-[48px] text-tokio smallPhone:h-[64px] smallPhone:text-[64px]">
-            home
-          </span>
-        </Link>
-        <Link href="/releases" className="flex">
-          <span className="material-symbols-outlined h-[48px] w-full text-[48px] text-tokio smallPhone:h-[64px] smallPhone:text-[64px]">
-            breaking_news
-          </span>
-        </Link>
-        <Link href="/timetable" className="flex">
-          <span className="material-symbols-outlined h-[48px] w-full text-[48px] text-tokio smallPhone:h-[64px] smallPhone:text-[64px]">
-            calendar_month
-          </span>
-        </Link>
-        <Link href="/map" className="flex">
-          <span className="material-symbols-outlined h-[48px] w-full text-[48px] text-tokio smallPhone:h-[64px] smallPhone:text-[64px]">
-            map
-          </span>
-        </Link>
-        <div onClick={toggle}>
-          <span
-            className="material-symbols-outlined h-[48px] w-full cursor-pointer text-[48px] text-tokio smallPhone:h-[64px] smallPhone:text-[64px]"
-            onClick={toggle}
-          >
-            more_horiz
-          </span>
-        </div>
-      </div>
       <div
         className={`${
           isOpen ? "visible" : "invisible"
@@ -147,5 +111,4 @@ const Navbar = () => {
     </div>
   )
 }
-
 export default Navbar
