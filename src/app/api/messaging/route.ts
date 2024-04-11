bf02ec81f2e839f3c512535379358d66a5d2f4dd
peedudu <66695333+peedudu@users.noreply.github.com>
2024-04-11 16:48:45 +0300

    Feat: Now saves the users email and the releases with timestamp in db

--- src/app/api/messaging/route.ts
+++ src/app/api/messaging/route.ts
@@ -1,13 +1,22 @@
 import { NextRequest, NextResponse } from "next/server"
 import {
-sendMessages
+    sendMessages, saveMessage, getDecodedClaims
 } from "@/firebaseAdmin"
 
-
 export async function POST(request: NextRequest) {
-    const data = await request.json()
-    await sendMessages(data.title, data.message, data?.topic, data.page).catch((e) => {
+    try {
+        const user = await getDecodedClaims(request.cookies.get("session")?.value || "")
+        if (!user) {
+            throw new Error("User not found");
+        }
+        const data = await request.json()
+        await Promise.all([
+            saveMessage(data.title, data.message, data.topic, data.page, user?.email || ""),
+            sendMessages(data.title, data.message, data?.topic, data.page)
+        ])
+
+        return NextResponse.json("Message sent")
+    } catch (e) {
         console.error("Error: " + e)
-    },)
-    return NextResponse.json("Message sent")
+    }
 }
