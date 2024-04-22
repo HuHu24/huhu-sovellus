758c9f9805fe7550b87500e25563be6c4f97f7b0
peedudu <66695333+peedudu@users.noreply.github.com>
2024-04-23 00:01:55 +0300

    Bug: Fixed an issue where a response wasnt given onto the request

--- src/app/api/auth/notifications/route.ts
+++ src/app/api/auth/notifications/route.ts
@@ -2,17 +2,17 @@ import { NextRequest, NextResponse } from "next/server"
 import { subscribeToTopic } from "@/firebaseAdmin"
 
 export async function POST(request: NextRequest) {
-  try {
     const result = (await request.json()) as { subcamp: number; token: string }
 
     await Promise.all([
       subscribeToTopic(result.token, result.subcamp.toString()),
       subscribeToTopic(result.token, "Kaikki"),
-    ])
+    ]).then(() => {
+        return NextResponse.json("Notification enabled")
 
-    return NextResponse.json("Notification enabled"), { status: 200 }
-  } catch (error) {
-    console.error(error)
-    return NextResponse.json("Something went wrong")
-  }
+    }).catch((error) => {
+        console.error(error)
+        return NextResponse.json("Error occurred when processing custom claim. Check request body formatting and data")
+    })
+    return NextResponse.json("Error occurred when processing custom claim. Check request body formatting and data")
 }
