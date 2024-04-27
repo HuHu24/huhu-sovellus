1f15f0c173a8cd0c8e57453cf09fc63fa768cb06
Aromi <aaro.heroja@gmail.com>
2024-04-27 18:26:49 +0300

    Sovellusmatskut #118 (#152)
    * feat: placeholder subcamp info
    
    * feat: dropdowns to navbar
    
    * feat: added static content
    
    * feat: added admin navigation page back
    
    * feat: improved subcamp page

---
+++ src/app/info/subcamp/page.tsx
@@ -0,0 +1,107 @@
+import Link from "next/link"
+
+export default function Home() {
+  return (
+    <div className="m-5 flex flex-col gap-3">
+      <div className="w-full rounded-xl bg-oslo p-2">
+        <h1 className="text-2xl font-bold">Komodo</h1>
+        <p>Moikkarallaa ja tervetuloa Komodoon!</p>
+        <br />
+        <p>
+          Tästä lähtee elämysmatka, jonka aikana majoitutte keskellä vuoria ja
+          viidakoita, Komodon kansallispuistossa. Mukananne seikkailevat liskot
+          ja lohikäärme!
+        </p>
+        <br />
+        <p>
+          Pue päälleesi vaaleanpunaista ja liity liskodiskoon! Karaokemestareina
+          toimimme me, Venla ja Lauri. Huhulla nähdään!
+        </p>
+        <p></p>
+        <br />
+      </div>
+      <div className="w-full rounded-xl bg-oslo p-2">
+        <h1 className="text-2xl font-bold">Central Park</h1>
+        <div>
+          <p>
+            Tervetuloa unohtumattomalle seikkailulle Central Park -alaleiriin,
+            missä meidät ympäröi Pohjois-Amerikan monipuolinen
+            kulttuuricocktail! Valmistaudu kiehtovaan matkaan, joka vie meidät
+            kanadalaiselta leirinuotiolta USA:n hektisiin suurkaupunkeihin ja
+            Meksikoon tutustumaan sen värikkääseen kulttuuriin. Matkaoppaina me,
+            eli Kata ja Julianna
+          </p>
+          <br />
+          <p>Nähään HuHulla!</p>
+        </div>
+      </div>
+      <div className="w-full rounded-xl bg-oslo p-2">
+        <h1 className="text-2xl font-bold">Rio</h1>
+        <p>
+          Kakaa! Kuuletko jo papukaijojen iloisen kutsun ja tunnetko Rion
+          lämpimän tuulenvireen kasvoillasi? Tervetuloa Rioon, iloiseen
+          alaleiriin josta ei värejä ja rytmikästä musiikkia puutu! Rion
+          sambakarnevaali on viisipäiväinen juhla Etelä-Amerikassa, siis vähän
+          kuten meidän leirimmekin. Hyppää siis kulkueeseen mukaan ja tule
+          kokemaan unohtumaton HuHu kanssamme!
+        </p>
+        <br />
+        <p>Innolla leiriä odottaen, Siiri ja Veera</p>
+      </div>
+      <div className="w-full rounded-xl bg-oslo p-2">
+        <h1 className="text-2xl font-bold">Bondi Beach</h1>
+        <p>
+          Kutsuuko sekoitus rentoutumista, aaltojen surffausta ja vilkasta
+          rantaelämää nimeäsi? Bondi Beach houkuttelee vierailijoita ympäri
+          maailman sen upeiden hiekkarantojen, kristallinkirkkaan veden ja
+          kauniin luonnon helmaan, jotka heijastavat paikan rentoa ja boheemia
+          tunnelmaa.
+        </p>
+        <br />
+        <p>
+          Tämä on ehdottomasti pysähdyspaikka kaikille, jotka haluavat kokea
+          Australialaisen rantakulttuurin sydämen sykkeen!!
+        </p>
+        <br />
+        <p>Matkaoppaina toimivat Nelli ja Laura😎</p>
+      </div>
+      <div className="w-full rounded-xl bg-oslo p-2">
+        <h1 className="text-2xl font-bold">Matera</h1>
+        <p>
+          Syöksy kanssamme Italian eteläosiin pieneen Materan kaupunkiin, joka
+          on suosittu yhteisöllisistä toreistaan ja upeista nähtävyyksistä,
+          jotka eivät todellakaan jätä kylmäksi! Tule kanssamme tutustumaan
+          Euroopan laajaan kulttuuriin ja sukeltamaan lämminhenkisiin
+          nuotiohetkiin sitruunapuiden alle!
+        </p>
+        <br />
+        <p>
+          Matkaoppaina pitkin kesäistä Eurooppaa toimii Mona, Annu ja upea
+          maskottimme, joka odottaa jo kuumeisesti HuHua!🐻Nähdään siellä!
+        </p>
+      </div>
+      <div className="w-full rounded-xl bg-oslo p-2">
+        <h1 className="text-2xl font-bold">Tekjäleiri Aboa</h1>
+        <p>
+          Tunnetko jo purevan pakkasen poskillasi ja kuuletko lumen ja jään
+          narskuvan jalkojesi alla? Tervetuloa tutkimusmatkalle hyiseen Aboaan,
+          jossa pingviinit asuvat naapurissa ja jäätikköä on
+          silmänkantamattomiin!
+        </p>
+        <br />
+        <p>
+          Vaikka tekijöillä varmasti riittääkin HuHulla yhtä sun toista puuhaa,
+          Aboassa myös tekijät pääsevät omalle tutkimusmatkalleen maapallon
+          artktisimpaan kolkkaan. Pukeudu siis lämpimästi ja valmistaudu
+          seikkailuun😎
+        </p>
+        <br />
+        <p>
+          Tutkimusryhmän johtajina toimimme me, Essi ja Aava. Nähdään HuHulla!
+        </p>
+        <br />
+      </div>
+      <div className="h-[70px]"></div>
+    </div>
+  )
+}
