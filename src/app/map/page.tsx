11d148e23a8195e75c5aad15c8bd519ce97a3084
Aaro <aaro.heroja@gmail.com>
2024-05-02 19:56:11 +0300

    feat: added map

---
+++ src/app/map/page.tsx
@@ -0,0 +1,14 @@
+export default function Map() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="max-w-full md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl">
+        <img
+          src="/map.jpg"
+          alt="HuHun leirialueen kartta"
+          className="h-auto w-full"
+          style={{ touchAction: "manipulation" }} // Ensure proper touch handling
+        />
+      </div>
+    </div>
+  )
+}
