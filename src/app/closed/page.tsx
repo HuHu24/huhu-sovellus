434485d819b3f6d1f525fa63f1a484ddfdd6395e
Aromi <aaro.heroja@gmail.com>
2024-05-19 21:14:35 +0300

    Update page.tsx

--- src/app/closed/page.tsx
+++ src/app/closed/page.tsx
@@ -2,7 +2,7 @@ const Closed = () => {
   return (
     <div className="absolute z-50 h-screen w-screen bg-helsinki">
       <div className="flex h-full flex-col place-content-center p-5">
-        <img claasName="max-w-[500px]" src={"/huhuymp.png"} alt={"HuHu logo"} />
+        <img className="max-w-[500px]" src={"/huhuymp.png"} alt={"HuHu logo"} />
         <h1 className="mb-2 text-center text-5xl font-bold">
           Sovellus ei ole vielä käytössä
         </h1>
