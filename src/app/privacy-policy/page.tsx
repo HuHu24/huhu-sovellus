261c1b5dc98c0fcbebfe250c2fc41cd7946855f1
Aaro <aaro.heroja@gmail.com>
2024-05-22 17:01:17 +0300

    feat: privacy policy and terms

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -14,7 +14,147 @@ export default function Map() {
           </div>
         </div>
       </div>
-      <div>Tietosuojaseloste tulee tänne</div>
+      <div className="p-3">
+        Tässä tietosuojaselosteessa kerromme, miten Lounais-Suomen Partiopiiri
+        ry ("Rekisterinpitäjä") yhteistyössä Google LLC:n ("Tietojen
+        käsittelijä") kanssa käsittelevät HuHu-sovelluksen käyttäjien
+        ("Rekisteröity") henkilötietoja ja miten henkilötietojen käsittelyyn voi
+        vaikuttaa. Rekisterinpitäjä ja Tietojen käsittelijä noudattavat
+        toiminnassaan soveltuvaa tietosuojalainsäädäntöä.
+        <br />
+        <br />
+        <strong>Rekisterinpitäjä</strong>
+        <br />
+        Lounais-Suomen Partiopiiri ry (2197948-1)
+        <br />
+        Itsenäisyydenkatu 44
+        <br />
+        28130 Pori
+        <br />
+        partiotoimisto.lsp@partio.fi
+        <br />
+        <br />
+        <strong>Tietojen käsittelijä</strong>
+        <br />
+        Google LLC (0907346-8) <br />
+        1600 Amphitheatre Parkway <br />
+        Mountain View CA 90430 <br />
+        UNITED STATES <br />
+        <br />
+        <strong>Kerättävät henkilötiedot</strong>
+        <br />
+        Rekisterinpitäjä ja Tietojen käsittelijä käsittelevät henkilötietoja
+        vain niiltä osin, kuin se on tarpeellista tässä tietosuojaselosteessa
+        kuvattujen käyttötarkoitusten toteuttamiseksi. Kerättävät henkilötiedot
+        ja niiden käsittelyn laajuus vaihtelevat Rekisterinpitäjän ja
+        Rekisteröidyn välisestä suhteesta, annetuista luvista sekä Rekisteröidyn
+        käyttämän selaimen yksityisyysasetuksista.
+        <br />
+        <br />
+        <strong>Asiakas- ja osallistujatiedot</strong>
+        <br />
+        Tiedot kerätään Rekisteröidyltä hänen rekisteröityessää
+        HuHu-sovellukseen. Alla mainituista henkilötietoryhmistä kerätään
+        kulloinkin tarpeelliset tiedot. Osa tiedoista voi olla vapaaehtoisia.
+        <br />
+        <ul className="ml-7 list-disc">
+          <li>perustiedot, kuten nimi</li>
+          <li>yhteystiedot, kuten sähköpostiosoite</li>
+          <li>
+            tapahtumaosallistumiseen liittyvät tarkentavat tiedot, kuten
+            osallistujaryhmät, taustatiedot tai -tapahtumaosallistumiseen
+            liittyvät valinnat
+          </li>
+          <li>mahdolliset Rekisteröidyn antamat muut lisätiedot</li>
+        </ul>
+        <br />
+        <br />
+        <strong>
+          Henkilötietojen käyttötarkoitukset ja käsittelyn oikeusperusta
+        </strong>
+        <br />
+        Rekisterinpitäjä käyttää kerättyjä henkilötietoja erilaisiin
+        asiakassuhteen hoitamisen kannalta välttämättömiin toimenpiteisiin,
+        kuten HuHu-sovelluksen toimittamiseen. Henkilötietojen käsittely
+        perustuu Rekisteröidyn ja Rekisterinpitäjän väliseen sopimukseen
+        HuHu-sovelluksen toimittamisesta.
+        <br />
+        <br />
+        <strong>Henkilötietojen jakaminen ja luovuttaminen</strong> <br />
+        Rekisterinpitäjä ja Tietojen käsittelijä ovat sopineet keskenään
+        henkilötietojen käsittelystä tämän tietosuojaselosteen ja soveltuvan
+        tietosuojalainsäädännön kuvaamalla tavalla. Tietojen käsittelijä toimii
+        tietojen käsittelijänä ja vain Rekisterinpitäjän lukuun. Tietojen
+        käsittelijä ei luovuta kolmannelle osapuolelle, ellei Rekisterinpitäjä
+        näin erikseen kirjallisesti ohjeista.
+        <br />
+        <br />
+        Rekisterinpitäjä tai Tietojen käsittelijä voivat luovuttaa
+        henkilötietoja kolmansille osapuolille, mikäli soveltuva lainsäädäntö
+        näin velvoittaa tai jos se on välttämätöntä Rekisterinpitäjän, Tietojen
+        käsittelijän tai Rekisteröidyn oikeuksien tai turvallisuuden
+        toteuttamiseksi. <br />
+        <br />
+        <strong>
+          Henkilötietojen siirtäminen EU/ETA-alueen ulkopuolelle
+        </strong>{" "}
+        <br />
+        Rekisterinpitäjä tai Tietojen käsittelijä eivät pääsääntöisesti siirrä
+        henkilötietoja EU/ETA-alueen ulkopuolelle, eikä henkilötietoja
+        muutoinkaan käsitellä EU/ETA-alueen ulkopuolella. Mikäli tietoja
+        poikkeuksellisesti siirretään EU/ETA-alueen ulkopuolelle, tietoja
+        käsitellään tämän tietosuojaselosteen ja lainsäädännön edellyttämällä
+        tavalla. <br />
+        <br />
+        <strong> Evästeiden käyttö</strong> <br />
+        HuHu-sovelluksen käyttökokemuksen parantamiseksi, sekä käytön
+        seuraamiseksi ja helpottamiseksi käytämme evästeitä (cookies). Evästeet
+        mahdollistavat lyhyiden tekstimuotoisten tietojen tallentamisen
+        käyttäjän selaimeen myöhempää käyttöä varten. <br />
+        <br />
+        <strong> Sijaintitietojen kerääminen ja käyttö</strong>
+        <br />
+        Rekisterinpitäjä tai Tietojen käsittelijä eivät pääsääntöisesti kerää
+        tai käytä Rekisteröityjen tarkkoja sijaintitietoja. <br />
+        <br />
+        <strong> Henkilötietojen säilyttäminen</strong>
+        <br />
+        Rekisterinpitäjä tai Tietojen käsittelijä säilyttävät kaikkea tietoa
+        enimmillään 3 kuukauden ajan Rekisteröidystä. Voimassa oleva kirjanpito-
+        tai muu pakottava lainsäädäntö voi kuitenkin velvoittaa
+        Rekisterinpitäjän tai Tietojen käsittelijän säilyttämään tietoja tätä
+        pidempään. Tällaisissa tilanteissa noudatetaan lainsäädännön määrittämiä
+        säilytysaikoja. <br />
+        <br />
+        <strong> Rekisteröidyn oikeudet ja vaikuttamismahdollisuudet</strong>
+        <br />
+        Rekisteröidyllä on oikeus saada pääsy omiin henkilötietoihinsa sekä
+        oikeus tarkistaa ja korjata itseään koskevia henkilötietoja. Lisäksi
+        Rekisteröidyllä on oikeus pyytää itseään koskevien henkilötietojen
+        poistamista niiltä osin, kuin se on muun lainsäädännön puitteissa
+        mahdollista. Rekisteröidyllä on oikeus myös siirtää itseään koskevat
+        henkilötiedot toiselle Rekisterinpitäjälle. <br />
+        <br />
+        Näiden oikeuksien käyttöä koskevat pyynnöt tulee esittää
+        Rekisterinpitäjälle tässä tietosuojaselosteessa annettujen
+        yhteystietojen mukaisesti. <br />
+        <br />
+        <strong> Tietoturva</strong>
+        <br />
+        Rekisterinpitäjä ja Tietojen käsittelijä huolehtivat henkilötietojen
+        tietoturvallisesta käsittelystä asianmukaisin fyysisin ja teknisin
+        tietoturvatoimenpitein suojataksemme tietoja katoamiselta,
+        tuhoutumiselta, väärinkäytöksiltä ja oikeudettomalta käytöltä ja
+        luovutukselta. Rekisterinpitäjä ja Tietojen käsittelijä pyrkivät
+        tietoturvalliseen henkilötietojen käsittelyyn esimerkiksi rajaamalla
+        pääsyn henkilötietoihin ja varmistamalla, että työntekijät ja
+        vapaaehtoiset käyttävät henkilötietoja annettujen ohjeiden, sopimusten
+        ja lainsäädännön mukaisesti.
+        <br />
+        <br />
+        <br />
+        <br />
+      </div>
     </>
   )
 }
