f4d56108f7bb58f5f3effcc69e2a693166f07c13
peedudu <66695333+peedudu@users.noreply.github.com>
2024-05-27 12:18:04 +0300

    Added song lyrics to the app under /info/song-lyrics

---
+++ src/app/info/song-lyrics/page.tsx
@@ -0,0 +1,92 @@
+export default function Home() {
+  return (
+    <div className="mx-auto w-[90%] rounded-xl bg-oslo ">
+      <div className="m-2 mx-auto flex h-full w-[70%] justify-items-center align-middle">
+        <pre className="mt-2 whitespace-pre-line text-left font-poppins text-xl text-ateena">
+          Vuosikausien unelma
+          <br />
+          Pitkä on odottavan aika
+          <br />
+          Ja nyt
+          <br />
+          Ollaan täällä
+          <br />
+          Siniset huivit päällä
+          <br />
+          Huhu koittanut on vihdoinkin
+          <br />
+          <br />
+          Monta kertaa jo ovella
+          <br />
+          Mut vasta nyt ikää on tarpeeks
+          <br />
+          Ja nyt
+          <br />
+          Kokee saan
+          <br />
+          mist nuotiol puhutaan
+          <br />
+          Täyttyy unelmamme vihdoinkin
+          <br />
+          <br />
+          Kuulen huudon
+          <br />
+          Ahtelan
+          <br />
+          <br />
+          Kun me Hu Hu
+          <br />
+          Huudetaan
+          <br />
+          Niin siiville nousee
+          <br />
+          Hu Hu
+          <br />
+          Niinku viimeistä päivää
+          <br />
+          Me lauletaan
+          <br />
+          Leiri kaikista suurin
+          <br />
+          Kaikuu tää tuuliin
+          <br />
+          <br />
+          Oi mikä aika olla elossa
+          <br />
+          Ja mikä rikkaus on tuntee
+          <br />
+          Kun tää
+          <br />
+          Laulu raikaa
+          <br />
+          Tässä jengis on taikaa
+          <br />
+          Näytetään nyt mihin meistä on
+          <br />
+          <br />
+          Tästä lauletaan lauluja
+          <br />
+          Vielä monen vuoden jälkeen
+          <br />
+          Kun tää
+          <br />
+          Huhu on, oi huhu on on on on
+          <br />
+          Huhu on niin ihana
+          <br />
+          <br />
+          Kerto x 2
+          <br />
+          Alkaa
+          <br />
+          Hu hu
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+        </pre>
+      </div>
+    </div>
+  )
+}
